f7dc567a5ad6fcea0b97e866eb14633cc7e22477
Borys Makhlin <borysmakhlin@Boryss-MacBook-Pro.local>
2025-07-11 12:02:00 +0200

    calendar poc

--- skedly-ui/src/app/app.ts
+++ skedly-ui/src/app/app.ts
@@ -1,12 +1,104 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalendarOptions, DateSelectArg, EventApi, EventClickArg, EventInput} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import {FullCalendarModule} from '@fullcalendar/angular';
+
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: '1',
+    title: 'All-day event',
+    start: TODAY_STR
+  },
+  {
+    id: '2',
+    title: 'Timed event',
+    start: TODAY_STR + 'T00:00:00',
+    end: TODAY_STR + 'T03:00:00'
+  },
+  {
+    id: '3',
+    title: 'Timed event',
+    start: TODAY_STR + 'T12:00:00',
+    end: TODAY_STR + 'T15:00:00'
+  }
+];
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FullCalendarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('skedly-ui');
+
+  calendarEvents = signal(INITIAL_EVENTS);
+
+  calendarOptions: CalendarOptions = {
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    nowIndicator: true,
+    datesSet: (e) => {
+      console.log('DATES SET, e:', e);
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: (e) => {
+      console.log('EVENTS SET, e:', e); //?
+    },
+    eventAdd: (e) => {
+      console.log('EVENT ADD, e:', e);
+    },
+    eventChange: (e) => {
+      console.log('EVENT CHANGE, e:', e);
+    },
+    eventRemove: (e) => {
+      console.log('EVENT REMOVE, e:', e);
+    },
+    select: this.handleDateSelect.bind(this),
+  };
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: '10',
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
 }
