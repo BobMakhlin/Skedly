153d8194ff2e560439ed223c3aaa729ff57b6486
Borys Makhlin <borysmakhlin@Boryss-MacBook-Pro.local>
2025-07-12 09:22:02 +0200

    add skedly api

---
+++ skedly-api/src/main/java/io/bobmakhlin/skedlyservice/SkedlyApiApplication.java
@@ -0,0 +1,13 @@
+package io.bobmakhlin.skedlyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkedlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkedlyApiApplication.class, args);
+    }
+
+}
