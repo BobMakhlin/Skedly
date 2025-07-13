c0788a338225bad96e9cd09cd5ef7f9e4ea92d23
Borys Makhlin <borysmakhlin@Boryss-MacBook-Pro.local>
2025-07-13 18:56:36 +0200

    month calendar component

---
+++ skedly-ui/src/app/features/month-calendar/month-calendar.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, Output, signal} from '@angular/core';
+import {FullCalendarModule} from '@fullcalendar/angular';
+import {CalendarOptions, EventInput} from '@fullcalendar/core';
+import {createMonthCalendarOptions} from './month-calendar.config';
+
+@Component({
+  selector: 'app-month-calendar',
+  imports: [
+    FullCalendarModule
+  ],
+  template: `
+    <full-calendar [events]="events()" [options]="calendarOptions">
+      <ng-template #eventContent let-arg>
+        <b>{{ arg.timeText }}</b>
+        <i>{{ arg.event.title }}</i>
+      </ng-template>
+    </full-calendar>
+  `,
+})
+export class MonthCalendarComponent {
+  @Input({ required: true }) events = signal<EventInput[]>([]);
+  @Output() public lazyLoad: EventEmitter<{
+    start: Date;
+    end: Date;
+  }> = new EventEmitter();
+  @Output() public addClick: EventEmitter<void> = new EventEmitter();
+  @Output() public eventClick: EventEmitter<string> = new EventEmitter();
+
+  calendarOptions: CalendarOptions = createMonthCalendarOptions({
+    onAddClick: () => this.addClick.emit(),
+    onLazyLoad: ({start, end}) => this.lazyLoad.emit({start, end: end}),
+    onEventClick: (id) => this.eventClick.emit(id)
+  });
+}
---
+++ skedly-ui/src/app/features/month-calendar/month-calendar.config.ts
@@ -0,0 +1,35 @@
+import {CalendarOptions} from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+export interface MonthCalendarHandlers {
+  onAddClick(): void;
+
+  onLazyLoad(range: { start: Date; end: Date }): void;
+
+  onEventClick(id: string): void;
+}
+
+export function createMonthCalendarOptions(
+  handlers: MonthCalendarHandlers
+): CalendarOptions {
+  return {
+    plugins: [dayGridPlugin, interactionPlugin],
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'customAddEvent'
+    },
+    customButtons: {
+      customAddEvent: {
+        text: 'Add Event',
+        click: handlers.onAddClick,
+      }
+    },
+    datesSet: (args) => handlers.onLazyLoad({start: args.start, end: args.end}),
+    eventClick: (args) => handlers.onEventClick(args.event.id),
+    weekends: true,
+    height: '95vh',
+    initialView: 'dayGridMonth',
+  };
+}
