7843cc5effd2e1ea03c093912a24aec33dd32b91
Borys Makhlin <borysmakhlin@Boryss-MacBook-Pro.local>
2025-07-15 14:38:42 +0200

    comment

--- skedly-ui/src/app/core/utils/date-time.utils.ts
+++ skedly-ui/src/app/core/utils/date-time.utils.ts
@@ -1,5 +1,9 @@
 import {DateTime} from 'luxon';
 
+
+// This solution is problematic:
+// Consider using moment lib and adapter to make it work with mat-datetimepicker.
+
 export function applyTimeZone(utcIso: string, timeZone: string): Date {
   const dt = DateTime.fromISO(utcIso, {zone: timeZone});
   return DateTime.local(
